a626a4ea1b0bcb6afbe550ddff6d193c145282e5
Sofie <63377159+SofieBrink@users.noreply.github.com>
2024-10-18 22:50:51 +0200

    Setup KSPBuildTools

--- Source/Modules/ModuleSEPEngineGUI.cs
+++ Source/Modules/ModuleSEPEngineGUI.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using TundraExploration.Modules;
+//using TundraExploration.Modules;
 
 namespace StarshipExpansionProject.Modules
 {
