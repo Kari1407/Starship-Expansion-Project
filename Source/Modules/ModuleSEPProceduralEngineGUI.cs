015cc5b63d45e4345018d990dda1c7f57f4b345e
Sofie <63377159+SofieBrink@users.noreply.github.com>
2024-12-23 18:18:01 +0100

    GUI New engine Module

--- Source/Modules/ModuleSEPProceduralEngineGUI.cs
+++ Source/Modules/ModuleSEPProceduralEngineGUI.cs
@@ -4,10 +4,10 @@ using System.Collections.Generic;
 using KSPCommunityFixes;
 using System.Linq;
 using StarshipExpansionProject.Utils.DataTypes;
+using KSP.Localization;
 // TODO: Remove unused items from State dict, probably in OnLoad? 
 //       Throw error if State dict already contains a key at load time
 //       enable relationships
-//       GUI
 //       Symmetry
 //       Actually Disable tranforms
 //       Actually toggle nodes
@@ -17,13 +17,16 @@ namespace StarshipExpansionProject.Modules
     // Part Module
     public class ModuleSEPProceduralEngineGUI : PartModule
     {
+        #region Static strings
         // Static strings
         public const string MODULENAME = nameof(ModuleSEPProceduralEngineGUI);
 
         public const string CustomSelectableNodeName = "CUSTOMSELECTABLE";
         public const string EngineSetNodeName = "ENGINESET";
         public const string EngineTypeNodeName = "ENGINETYPE";
+        #endregion
 
+        #region Serialized Fields
         // Serialized Fields
         [SerializeField]
         public List<CustomSelectable> customSelectables;
@@ -36,17 +39,129 @@ namespace StarshipExpansionProject.Modules
 
         [SerializeField]
         public List<Transform> transforms;
+        #endregion
 
+        #region KSP Fields
         // KSP Fields
         [KSPField]
         public string transformNameSeperator = string.Empty;
+
+        [KSPField]
+        public float engineSize = 20;
 
         [KSPField(isPersistant = true)]
         public PersistentDictionaryValueTypeKey<string, PersistentDictionaryValueTypes<string, bool>> State = new PersistentDictionaryValueTypeKey<string, PersistentDictionaryValueTypes<string, bool>>();
 
         [KSPField(isPersistant = true)]
-        public string activeEngineType = string.Empty;
+        public int activeEngineType = 0;
 
+        [KSPEvent(guiActive = false, guiActiveEditor = true, guiActiveUnfocused = false, guiName = "#LOC_SEP_SelectEnginesGUIName")]
+        public void OpenGui()
+        {
+            isVisible = true;
+        }
+        #endregion
+
+        #region Private UI Fields
+        // Private UI Fields
+        private Rect windowRect = new Rect(250, 200, 250, 376);
+        private bool isDragging = false;
+        private bool isVisible = false;
+        private Vector2 customSelectablesScrollPosition = Vector2.zero;
+        private string uiName = Localizer.GetStringByTag("#LOC_SEP_GUILabelName");
+        private float uiWidth = 236;
+        private static int highestuiIndex = -1;
+        private int? _uiIndex;
+        private int uiIndex
+        {
+            get
+            {
+                if (_uiIndex is null)
+                {
+                    highestuiIndex++;
+                    _uiIndex = highestuiIndex;
+                }
+                return (int) _uiIndex;
+            }
+        }
+        private float? _uiScale;
+        private float uiScale
+        {
+            get
+            {
+                if (_uiScale == null)
+                {
+                    var tmpPositions = EnginePositionsDict.SelectMany(ep => ep.Value);
+                    var maxExtent = tmpPositions.Max(t => t.magnitude);
+                    _uiScale = (uiWidth / 2 - engineSize / 2 - 3) / maxExtent;
+                }
+
+                return (float) _uiScale;
+            }
+        }
+
+        #endregion
+
+        #region Private Fields
+        // Private Fields
+        private Dictionary<int, List<CustomSelectable>> _selectablesDict;
+        private Dictionary<int, List<CustomSelectable>> SelectablesDict
+        {
+            get
+            {
+                if (_selectablesDict == null)
+                {
+                    _selectablesDict = new Dictionary<int, List<CustomSelectable>>();
+                    for (int i = 0; i < engineTypes.Count; i++)
+                    {
+                        _selectablesDict.Add(i, customSelectables.Where(s => engineTypes[i].CustomSelectableNames.Contains(s.name)).ToList());
+                    }
+                }
+                return _selectablesDict;
+            }
+        }
+
+        private Dictionary<int, List<EngineSet>> _engineSetDict;
+        private Dictionary<int, List<EngineSet>> EngineSetDict
+        {
+            get
+            {
+                if (_engineSetDict == null)
+                {
+                    _engineSetDict = new Dictionary<int, List<EngineSet>>();
+                    for (int i = 0; i < engineTypes.Count; i++)
+                    {
+                        _engineSetDict.Add(i, engineSets.Where(s => engineTypes[i].EngineSetNames.Contains(s.name)).ToList());
+                    }
+                }
+                return _engineSetDict;
+            }
+        }
+
+        private Dictionary<string, List<Vector2>> _enginePositionsDict;
+        private Dictionary<string, List<Vector2>> EnginePositionsDict
+        {
+            get
+            {
+                if (_enginePositionsDict == null)
+                {
+                    _enginePositionsDict = new Dictionary<string, List<Vector2>>();
+                    for (int i = 0; i < engineSets.Count(); i++)
+                    {
+                        var tmpTransformPositions = engineSets[i]
+                                                   .selectableItems
+                                                   .Select(t => part.transform
+                                                               .InverseTransformVector(transforms[t.transformIndexes[0]].position));
+                        _enginePositionsDict.Add(engineSets[i].name
+                                               , tmpTransformPositions.Select(p => new Vector2(p.x, p.z)).ToList());
+                    }
+                }
+                return _enginePositionsDict;
+            }
+        }
+        #endregion
+
+        #region KSP Methods
         // KSP Methods
         public override void OnLoad(ConfigNode node)
         {
@@ -56,7 +171,14 @@ namespace StarshipExpansionProject.Modules
             if (engineTypes == null) engineTypes = new List<EngineType>();
             if (transforms == null
              || node.HasNode(CustomSelectableNodeName)
-             || node.HasNode(EngineTypeNodeName)) transforms = new List<Transform>(); // this assumes b9ps sends a complete confnode, just modified
+             || node.HasNode(EngineTypeNodeName))
+            {
+                transforms = new List<Transform>(); // this assumes b9ps sends a complete confnode, just modified
+                _selectablesDict = null;
+                _engineSetDict = null;
+                _enginePositionsDict = null;
+                _uiScale = null;
+            }
 
             foreach (var tmpNode in node.GetNodes())
             {
@@ -79,13 +201,20 @@ namespace StarshipExpansionProject.Modules
             }
         }
 
+        public void OnGUI()
+        {
+            if (isVisible) windowRect = GUI.Window(uiIndex, windowRect, DrawUI, uiName);
+        }
+
         public void FixedUpdate()
         {
         }
         public void Start()
         {
         }
+        #endregion
 
+        #region Custom Methods
         // Custom Methods
         private bool ParseCustomSelectable(ConfigNode node)
         {
@@ -95,15 +224,25 @@ namespace StarshipExpansionProject.Modules
             // Fill toplevel object
             if (node.HasValue(nameof(tmpCustomSelectable.name)))
                 tmpCustomSelectable.name = node.GetValue(nameof(tmpCustomSelectable.name));
-            else vml.Add((VmlSeverity.Error, $"{EngineTypeNodeName} \"{tmpCustomSelectable.name}\" has no value {nameof(tmpCustomSelectable.name)}"));
+            else vml.Add((VmlSeverity.Error, $"{CustomSelectableNodeName} \"{tmpCustomSelectable.name}\" has no value {nameof(tmpCustomSelectable.name)}"));
+
+            if (node.HasValue(nameof(tmpCustomSelectable.guiName)))
+                tmpCustomSelectable.guiName = node.GetValue(nameof(tmpCustomSelectable.guiName));
+            else
+            {
+                tmpCustomSelectable.guiName = tmpCustomSelectable.name;
+                vml.Add((VmlSeverity.Information, $"{CustomSelectableNodeName} \"{tmpCustomSelectable.name}\" has no value {nameof(tmpCustomSelectable.guiName)}, using default: {tmpCustomSelectable.name}"));
+            }
 
             // This one is a little special because we don't need to store it in this object
             bool tmpDefaultState = true;
             if (node.HasValue("defaultState"))
                 tmpDefaultState = bool.Parse(node.GetValue("defaultState"));
-            else vml.Add((VmlSeverity.Information, $"{EngineSetNodeName} \"{tmpCustomSelectable.name}\" has no value defaultState, using default: {tmpDefaultState}"));
+            else vml.Add((VmlSeverity.Information, $"{CustomSelectableNodeName} \"{tmpCustomSelectable.name}\" has no value defaultState, using default: {tmpDefaultState}"));
 
             if (!State.ContainsKey(tmpCustomSelectable.name)) State.Add(tmpCustomSelectable.name, new PersistentDictionaryValueTypes<string, bool>());
+            if (!State[tmpCustomSelectable.name].ContainsKey(tmpCustomSelectable.name))
+                State[tmpCustomSelectable.name].Add(tmpCustomSelectable.name, tmpDefaultState);
 
             // Fill Items
             if (node.HasNode("ITEM"))
@@ -150,8 +289,6 @@ namespace StarshipExpansionProject.Modules
                     {
                         Debug.LogError($"[{MODULENAME}] {CustomSelectableNodeName} no transform found with name \"{tmpItem.nodeId}\"");
                     }
-                    if (!State[tmpCustomSelectable.name].ContainsKey(tmpItem.name))
-                        State[tmpCustomSelectable.name].Add(tmpItem.name, tmpDefaultState);
                     tmpItem.transformIndexes.AddRange(Enumerable.Range(transforms.Count, tmpTransforms.Length));
                     transforms.AddRange(tmpTransforms);
                     if (tmpItem.nodeId != string.Empty && tmpNodeTransform != null)
@@ -196,6 +333,14 @@ namespace StarshipExpansionProject.Modules
             if (node.HasValue(nameof(tmpEngineSet.name))) 
                 tmpEngineSet.name = node.GetValue(nameof(tmpEngineSet.name));
             else vml.Add((VmlSeverity.Error, $"{EngineSetNodeName} \"{tmpEngineSet.name}\" has no value {nameof(tmpEngineSet.name)}"));
+
+            if (node.HasValue(nameof(tmpEngineSet.guiName)))
+                tmpEngineSet.guiName = node.GetValue(nameof(tmpEngineSet.guiName));
+            else
+            {
+                tmpEngineSet.guiName = tmpEngineSet.name;
+                vml.Add((VmlSeverity.Information, $"{EngineSetNodeName} \"{tmpEngineSet.name}\" has no value {nameof(tmpEngineSet.guiName)}, using default: {tmpEngineSet.name}"));
+            }
 
             if (node.HasValue(nameof(tmpEngineSet.engineId)))
             {
@@ -347,12 +492,119 @@ namespace StarshipExpansionProject.Modules
                 part.AddAttachNode(node);
             }
         }
+
+        private void DrawUI(int windowIndex)
+        {
+            // Setup
+            GUI.DragWindow(new Rect(0, 0, windowRect.width - 20, 20));
+            float barButtonWidth = 60;
+            float barButtonHeight = 35;
+            
+            Rect closeButtonRect = new Rect(windowRect.width - 22, 2, 20, 20);
+            GUIStyle buttonStyle = GUI.skin.button;
+            buttonStyle.alignment = TextAnchor.MiddleCenter;
+            buttonStyle.fontSize = 15;
+            GUIStyle labelStyle = GUI.skin.label;
+            labelStyle.alignment = TextAnchor.MiddleCenter;
+            GUIStyle toggleStyle = GUI.skin.toggle;
+            toggleStyle.wordWrap = true;
+
+            GUI.skin.window.padding = new RectOffset(0, 0, 20, 0);
+
+            if (GUI.Button(closeButtonRect, "X", buttonStyle))
+            {
+                isVisible = false;
+            }
+
+            // Main Window
+            GUILayout.BeginVertical();
+            GUILayout.Space(5);
+
+            // Top Bar
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("<"
+                               , buttonStyle
+                               , GUILayout.Width(barButtonWidth)
+                               , GUILayout.Height(barButtonHeight)))
+            {
+                UpdateActiveEngineType(-1);
+            }
+
+            GUILayout.Label(engineTypes[activeEngineType].name
+                          , labelStyle
+                          , GUILayout.Width(uiWidth - 2 * barButtonWidth)
+                          , GUILayout.Height(barButtonHeight));
+
+            if (GUILayout.Button(">"
+                               , buttonStyle
+                               , GUILayout.Width(barButtonWidth)
+                               , GUILayout.Height(barButtonHeight)))
+            {
+                UpdateActiveEngineType(1);
+            }
+            GUILayout.EndHorizontal();
+            GUILayout.Space(3);
+
+            // Custom Selectables
+            customSelectablesScrollPosition = GUILayout.BeginScrollView(customSelectablesScrollPosition
+                                                                      , GUI.skin.box
+                                                                      , GUILayout.Height(60));
+            GUILayout.BeginVertical();
+            foreach(var selectable in SelectablesDict[activeEngineType])
+            {
+                // Worry about disabling based on relationships.
+                if (GUILayout.Toggle(true, selectable.guiName, toggleStyle))
+                {
+                    //State[selectable.name][selectable.name] = !State[selectable.name][selectable.name];
+                    Debug.Log("Mock Toggle " + selectable.name);
+                }
+            }
+
+            GUILayout.EndVertical();
+            GUILayout.EndScrollView();
+            GUILayout.Space(3);
+
+            // Engines
+            //Rect canvasRect = GUILayoutUtility.GetRect(windowWidth - GUI.skin.window.border.horizontal*16, windowWidth - GUI.skin.window.border.horizontal*6);
+            //GUI.Box(canvasRect, GUIContent.none, GUI.skin.box);
+            GUILayout.BeginVertical(GUI.skin.box, GUILayout.Height(uiWidth));
+            GUILayout.Label(GUIContent.none);
+            GUILayout.EndVertical();
+            if (Event.current.type == EventType.Repaint)
+            {
+                Rect canvasRect = GUILayoutUtility.GetLastRect();
+                foreach (var set in EngineSetDict[activeEngineType])
+                {
+                    for (int i = 0; i < set.selectableItems.Count; i++)
+                    {
+                        float tmpX = canvasRect.x + canvasRect.width / 2 + EnginePositionsDict[set.name][i].x * uiScale - engineSize / 2;
+                        float tmpY = canvasRect.y + canvasRect.height / 2 + EnginePositionsDict[set.name][i].y * uiScale - engineSize / 2;
+                        var tmpPosition = new Rect(tmpX, tmpY, engineSize, engineSize);
+                        if(GUI.Toggle(tmpPosition, true, GUIContent.none))
+                        {
+                            Debug.Log($"Mock Toggle {set.name} : {set.selectableItems[i].name}");
+                        }
+                    }
+                }
+            }            
+            GUILayout.Space(3);
+
+            GUILayout.EndVertical();
+        }
+
+        public void UpdateActiveEngineType(int dir)
+        {
+            activeEngineType = (activeEngineType + dir + engineTypes.Count) % engineTypes.Count;
+        }
+        #endregion
     }
 
+    #region Custom Data Classes
     // Custom Data Classes
     [Serializable]
     public class CustomSelectable : ScriptableObject
     {
+        public string guiName;
         public List<Relationship> relationships = new List<Relationship>();
         public List<SelectableItem> selectableItems = new List<SelectableItem>();
     }
@@ -360,6 +612,7 @@ namespace StarshipExpansionProject.Modules
     [Serializable]
     public class EngineSet : ScriptableObject
     {
+        public string guiName;
         public string engineId;
         public float singleEngineThrust;
         public float singleEngineMinThrust = 0;
@@ -397,4 +650,5 @@ namespace StarshipExpansionProject.Modules
         Warning,
         Information,
     }
+    #endregion
 }
